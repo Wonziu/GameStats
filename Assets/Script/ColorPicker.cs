8517b9c6669f4dab5f929af026eb1a283ab65283
Mateusz Leonowicz <leonwonziu@gmail.com>
2018-07-10 06:39:06 +0200

    Added base for color picker

---
+++ Assets/Script/ColorPicker.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorPicker : MonoBehaviour
+{
+    public Color pickedColor;
+
+    public Image colorPool;
+    public Image hue;
+
+    public GameObject poolKnob;
+    public GameObject result;
+    public GameObject hueKnob;
+
+    private void Start()
+    {
+        CreateHue();
+    }
+
+    private void CreateHue()
+    {
+        var texture = new Texture2D(1, 7);
+
+        var hueColors = new[] {
+            Color.red,
+            Color.yellow,
+            Color.green,
+            Color.cyan,
+            Color.blue,
+            Color.magenta,
+            Color.red 
+        };
+
+        for (int i = 0; i < hueColors.Length; i++)
+            texture.SetPixel(0, i, hueColors[i]);
+
+        texture.Apply();
+
+        hue.sprite = Sprite.Create(texture, new Rect(0, 0, 1, 6), new Vector2(0.5f, 0.5f));
+    }
+}
